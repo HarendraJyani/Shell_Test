2cffe1701169fd64c6a1d8f5dd9a01106160466a
HarendraJyani <harendra.jyani@quantiphi.com>
2018-06-12 09:47:35 +0530

    Just added more in it

--- AwsQ2.sh
+++ AwsQ2.sh
@@ -14,3 +14,4 @@ done
 
 aws ec2 run-instances --image-id ami-14c5486b --instance-type t2.micro --key-name ShellAssign --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=harrytransfer2},{Key=username,Value=harendrajyani},{Key=emailId,Value=harendra.jyani@quantiphi.com},{Key=Project,Value=PE_Train}]' --iam-instance-profile Arn=arn:aws:iam::488599217855:instance-profile/PE_trainee_Admin_role --user-data '#!/bin/bash
 aws s3 cp s3://harrytransfer1/random.txt random.txt' --region us-east-1
+#I have used my own key to launch two instances
